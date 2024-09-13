0f9bd8059ad7e55842124a397b93eda524ced85e
Mohamed20008 <mohamedabdelfatah2000max@gmail.com>
2024-09-13 14:03:47 +0300

    nothing

--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+# علاقه واحد بواحد
 
-# Create your models here.
-class Car(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+class Female(models.Model):
+    name_female = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.name_female
+class Male(models.Model):
+    name_male = models.CharField(max_length=50, null=True)
+    girl = models.OneToOneField(Female, on_delete=models.CASCADE)
+    # girl = models.OneToOneField(Female, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.name_male
+# علاقه واحد بكثير
+class Product (models.Model):
+    title = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    name = models.CharField(max_length=55, null=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+# علاقه كثير لكثير
+class Video (models.Model):
+    title = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return self.title
+class UserName(models.Model):
+    name = models.CharField(max_length=55, null=True)
+    watch = models.ManyToManyField(Video, null=True)
+    def __str__(self):
+        return self.name
--- products/models.py
+++ products/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-
+from datetime import datetime
+x =[
+    ('phone', 'phone'),
+    ('computer', 'computer')
+]
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -7,6 +11,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     # image = models.ImageField(upload_to='photos/%y/%m/%d')
     active = models.BooleanField(default=True)
+    category = models.CharField(max_length=50,choices=x, default='phone')
     
     def __str__(self):
         return self.name
@@ -15,3 +20,8 @@ class Product(models.Model):
         ordering=['name']
         # ordering=['price']
         # ordering=['-price']
+class Test (models.Model):
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
+    created = models.DateTimeField(null=True , default=datetime.now)
+1
